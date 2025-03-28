1f5b947b9652fd6804e60bb2609ec163698d7640
Nabil <nabil.ghayyda@quarch.com>
2025-03-28 17:24:00 +0000

    Added suggested fixes from review. - Re-worked live-monitoring example to use QIS API directly. - Simple streaming example now streams directly to file rather than using in-memory python data object. - Updated code comments. - Removed unnecessary code.

--- QisMultiDeviceStreamingExample.py
+++ QisMultiDeviceStreamingExample.py
@@ -1,15 +1,12 @@
 '''
-AN-032 - Application note demonstrating automated control of multiple power devices via QIS
+AN-032 - Application note demonstrating capture of power data from multile devices at the same time
 
-Automating via QIS is a lower overhead than running QPS (Quarch Power Studio) in full but still
-provides easy access to data for custom processing.  This example uses Quarchpy functions to set up
-multiple power devices for streaming via QIS.
+Automated capture allows data from multiple instruments to be gathered at the same time, and with lower overhead than running QPS (Quarch Power Studio).
+This example uses quarchpy functions to connect and pull data from a list of instruments on the network.
 
-There are several examples that run in series, these can be commented out if you want to simplify the actions:
-
-simple_multi_stream_example() - This example streams each modules stream data to a unique python data structure and then
-outputs the data to a csv file "{module-name}.csv"
-multi_device_live_monitoring_example - This example showcases live monitoring for select stream data
+We demonstrate two capture modes:
+simple_multi_stream_example() - This example streams each modules stream data to a CSV file for post-processing
+multi_device_live_monitoring_example() - This example showcases live monitoring for select stream data
 
 QIS is distributed as part of the Quarchpy python package and does not require separate install
 
@@ -21,20 +18,19 @@ QIS is distributed as part of the Quarchpy python package and does not require s
 
 1- Python (3.x recommended)
     https://www.python.org/downloads/
-2- Java 21, with JaxaFX
-    https://quarch.com/support/faqs/java/
-3- Quarchpy python package
+2- Quarchpy python package
     https://quarch.com/products/quarchpy-python-package/
-4- Quarch USB driver (Required for USB connected devices on windows only)
+3- Quarch USB driver (Required for USB connected devices on windows only)
     https://quarch.com/downloads/driver/
-5- Check USB permissions if using Linux:
+4- Check USB permissions if using Linux:
     https://quarch.com/support/faqs/usb/
 
 ########### INSTRUCTIONS ###########
 
 1. Connect multiple Quarch power modules via USB/Ethernet.
 2. Edit the script and hard-code the modules you'd like to stream with (edit myDeviceIDs list).
-3. Run the script and follow any instructions on the terminal
+3. Optionally select the channels you want to monitor for live data (edit channelsToMonitor dict).
+4. Run the script and follow any instructions on the terminal
 
 ####################################
 '''
@@ -42,13 +38,15 @@ import os
 
 # Import other libraries used in the examples
 import time  # Used for sleep commands
-# import logging  # Optionally used to create a log to help with debugging
-
-from quarchpy.device import *
-from quarchpy.qis import *
-
+import xml.etree.ElementTree as ET
+from dataclasses import dataclass
 from io import StringIO
 from threading import Thread
+# import logging  # Optionally used to create a log to help with debugging
+# QuarchPy imports
+from quarchpy.device import *
+from quarchpy.qis import *
+from quarchpy.user_interface import displayTable, visual_sleep
 
 # Global variables to store last values and stream status
 csv_data_io = []  # Store stream data in memory
@@ -57,31 +55,58 @@ stream_running = False
 
 # Hardcoded list to hold all module ID's you'd like to stream with.
 myDeviceIDs = [
-    'TCP:QTL2789-01-001',
-    'TCP:QTL2789-01-002',
+    # 'TCP:QTL2789-01-001',
+    # 'TCP:QTL2789-01-002',
     'TCP:QTL2582-01-005',
     'TCP:QTL2582-01-006',
-    'TCP:QTL2843-03-002',
-    'TCP:QTL2751-02-002',
-    'TCP:QTL2751-01-001'
+    'TCP:QTL2843-03-002'
+    # 'TCP:QTL2751-02-002',
+    # 'TCP:QTL2751-01-001'
 ]
+
+channels = []
+
+
+# Define a Channel object using a dataclass
+@dataclass
+class Channel:
+    name: str
+    group: str
+    units: str
+    max_t_value: int
+    data_position: int
+
+
+# List of channels to monitor.
+# The key represents the channel header from the output CSV, while the value is a user-friendly label for reporting purposes.
+channelsToMonitor = {
+    "L1_RMS mV": "L1 RMS Voltage",
+    "L1_RMS mA": "L1 RMS Current",
+    "Tot_PApp mVA": "Total Apparent Power"
+}
 
 
 def main():
+    """
+    This is the main function that starts QIS, Connections to Modules and calls the Example Functions.
+    :return: None
+    """
     # If required you can enable python logging, Quarchpy supports this and your log file
     # will show the process of scanning devices and sending the commands.  Just comment out
     # the line below.  This can be useful to send to quarch if you encounter errors
     # logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
-    print("\n\nQuarch application note example: AN-032")
-    print("---------------------------------------\n\n")
+    displayTable("Quarch application note example: AN-032")
 
-    # Start QIS (if it is already running, skip this step and also avoid closing it at the end)
-    print("Starting QIS...\n")
     closeQisAtEndOfTest = False
+    # Check if QIS is running locally (if it is already running, skip this step and also avoid closing it at the end)
     if isQisRunning() is False:
+        # Start QIS
+        print("Starting QIS...\n")
         startLocalQis()
         closeQisAtEndOfTest = True
+    else:
+        print("QIS already running on Host Machine")
 
     # Connect to the localhost QIS instance
     myQis = QisInterface()
@@ -106,23 +131,28 @@ def main():
         csv_data_io.append(StringIO())
 
     # Select one or more example functions to run, you can comment any of these out if you do not want to run them
-    simple_multi_stream_example(myPowerDevices)
+    # simple_multi_stream_example(myPowerDevices)
     multi_device_live_monitoring_example(myPowerDevices)
 
     if closeQisAtEndOfTest:
         closeQis()
 
-
-'''
-This example streams measurement data from each module to a file for each module, by default in the same folder as the script
-'''
-
-
 # Setup each power module and start streaming
 def simple_multi_stream_example(modules):
+    """
+    Starts streaming measurement data from multiple devices to separate files.
+
+    1. Sets the resampling rate for each module.
+    2. Starts recording and streams data to a file for 30 seconds.
+    3. Starts a separate thread to check the stream status once a second
+    4. Stops the stream.
+
+    :param modules: dict[int, quarchPPM]
+    :return: None
+    """
     global stream_running
 
-    print("\n Simple Multi Device Streaming Example \n")
+    print("\nSimple Multi Device Streaming Example\n")
 
     # Loop through all set devices in hard-coded list
     for i in range(len(myDeviceIDs)):
@@ -130,40 +160,62 @@ def simple_multi_stream_example(modules):
         modules[i].streamResampleMode("1mS")
 
         # Start the recording and set the stream duration to 30 seconds to ensure all modules stream for the same amount of time.
+        # Setting the fileName allows the API call to stream directly to the file.
         print(f"Started Recording on module: {myDeviceIDs[i]}\n")
-        modules[i].startStream(inMemoryData=csv_data_io[i], fileName=None, streamDuration=30)
+        # Create file name using the device id
+        file_name = f'{get_device_id(myDeviceIDs[i])}.csv'
+        modules[i].startStream(fileName=file_name, streamDuration=30)
+
+    stream_running = True
+
+    # Start a separate thread to check the stream status each second
+    thread = Thread(target=check_stream_status, args=(modules,))
+    thread.start()
 
     print("\nWait for 30 seconds...\n")
-    time.sleep(30)
+    visual_sleep(30)
 
+    # Ensure global variable is set too false to stop the live data coming through.
+    stream_running = False
+
+    # Loop through the devices and stop each device stream
     for i in range(len(myDeviceIDs)):
         # Stop the stream
         print(f"\nStopping the stream on module: {myDeviceIDs[i]}")
         modules[i].stopStream()
 
-        # Check stream status
-        check_stream_status(modules[i])
-
-        # Print & output the acquired data to a csv file "stream-date.csv"
-        process_qis_data(get_device_id(myDeviceIDs[i]), csv_data_io[i])
-
 
 def multi_device_live_monitoring_example(modules):
+    """
+    Starts live monitoring for multiple devices.
+
+    1. Sets the resampling rate and starts recording on each module.
+    2. Checks that the stream header contains the required channels.
+    3. Runs a background thread to read and print live data for 30 seconds.
+    4. Stops the stream and checks its status on each module.
+
+    :param modules: dict[int, quarchPPM]
+    :return: None
+    """
     global stream_running
 
-    print("\n Multi Device Live Monitoring Example \n")
+    print("\nMulti Device Live Monitoring Example\n")
 
     for i in range(len(myDeviceIDs)):
         print(f"\nSetting QIS resampling to 500mS on module {myDeviceIDs[i]}")
         modules[i].streamResampleMode("500mS")
 
-        # Start the recording and set the stream duration to 30 seconds to ensure all modules stream for the same amount of time.
+        # Start the recording
         print(f"Started Recording on module: {myDeviceIDs[i]}\n")
-        modules[i].startStream(inMemoryData=csv_data_io[i], fileName=None, streamDuration=30)
+        modules[i].sendAndVerifyCommand("rec stream")
+
+        # Check stream header contains channels you'd like to monitor
+        check_header_contains_channels_to_monitor(modules[i])
 
     stream_running = True
-    # Start a separate thread to read and cache stream data every second
-    thread = Thread(target=read_and_print_last_values, daemon=True)
+
+    # Start a separate thread to read and cache stream data at the specified interval
+    thread = Thread(target=read_and_print_last_values, args=(modules,))
     thread.start()
 
     print("\nWait for 30 seconds...\n")
@@ -175,78 +227,132 @@ def multi_device_live_monitoring_example(modules):
     for i in range(len(myDeviceIDs)):
         # Stop the stream
         print(f"\nStopping the stream on module: {myDeviceIDs[i]}")
-        modules[i].stopStream()
-
-        # Process final stream data and update cache
-        process_stream_data()
-
-        # Check stream status
-        check_stream_status(modules[i])
+        modules[i].sendCommand("rec stop")
 
 
-# Function to cache the last sample for each column
-def process_stream_data():
-    for i in range(len(csv_data_io)):
-        csv_data_io[i].seek(0)  # Rewind to the start of the CSV data
-        lines = csv_data_io[i].readlines()  # Read all lines
-        if lines:
-            headers = lines[0].strip().split(',')  # Extract headers from first line
-            last_line = lines[-1].strip().split(',')  # Extract the last line of data
-            for j, header in enumerate(headers):
-                if header != "" and header is not None:  # Only process non-empty headers
-                    last_values[f'{get_device_id(myDeviceIDs[i])} {header}'] = last_line[
-                        j]  # Cache the last sample for each valid column
+def process_stream_data(modules: dict[int, quarchPPM]):
+    """
+    Function to cache the stream data for each module.
+    :param modules: dict[int, quarchPPM]
+    :return:
+    """
+    # Iterate through each module in the provided list of modules.
+    for i in range(len(modules)):
+        # Send a command to get the streaming data in text format.
+        line = modules[i].sendCommand("stream text 1")
 
+        # Split the returned line into parts, removing leading/trailing whitespace.
+        # Skip the first part (assumed to be the timestamp) using slicing.
+        parts = line.strip().split()[1:]
 
-# API to request the last value by channel name
-def get_last_value(channel_name):
-    return last_values.get(channel_name, None)
+        # Convert the remaining parts to integers and store them as a sublist in last_values.
+        last_values[f'{get_device_id(myDeviceIDs[i])}'] = [int(x) for x in parts]
 
-
-# Continuously read and print last values during the stream every second
-def read_and_print_last_values():
+def read_and_print_last_values(modules: dict[int, quarchPPM], sleep_interval=0.4):
+    """
+    Continuously reads and prints the latest channel values for monitored devices at a specified interval.
+    :param modules: dict[int, quarchPPM]
+    :param sleep_interval: int
+    :return: None
+    """
+    # Loop continuously while the stream is active.
     while stream_running:
-        # Process the latest stream data to update the cache
-        process_stream_data()
+        # Update the cache with the most recent stream data for all modules.
+        process_stream_data(modules)
 
+        # Initialize an index to track the position in the channels list.
+        index = 0
+        # Iterate through each device in the list of device IDs.
         for device_id in myDeviceIDs:
-            # Get and print last values for L1 RMS Voltage, Current and Power
-            l1_rms_voltage = get_last_value(f"{get_device_id(device_id)} L1_RMS mV")
-            l1_rms_current = get_last_value(f"{get_device_id(device_id)} L1_RMS mA")
-            l1_p_app = get_last_value(f"{get_device_id(device_id)} Tot_PApp mVA")
+            # Ensure the device ID is in the correct format or retrieve the actual ID.
+            device_id = get_device_id(device_id)
+            if len(last_values[device_id]) != 0:
+                print(f'\n{device_id}')
+                # Loop through each channel to monitor and its descriptor.
+                for channel_key, channel_descriptor in channelsToMonitor.items():
+                    # Retrieve the corresponding channel object using the current index and channel key.
+                    channel = channels[index].get(channel_key)
+                    # Check if there is any data to process
+                    # Print the channel descriptor and the most recent value for this device and channel.
+                    print(f'{channel_descriptor}: {last_values[device_id][channel.data_position - 1]}', end=' ')
 
-            if l1_rms_voltage or l1_rms_current or l1_p_app:
-                print(
-                    f"{get_device_id(device_id)} L1 RMS Voltage: {l1_rms_voltage}, L1 RMS Current: {l1_rms_current}, Total Apparent Power: {l1_p_app}")
+            # Move to the next device's channels.
+            index += 1
+
+        # Print a blank line to separate outputs.
         print('\n')
+
+        # Pause execution for the specified interval before the next iteration.
+        time.sleep(sleep_interval)
+
+
+def check_stream_status(modules):
+    """
+    Function that checks the status of each modules stream to ensure each stream ran with no interruptions.
+    :param modules: dict[int, quarchPPM]
+    :return: None
+    """
+    while stream_running:
+        for module in modules:
+            stream_status = module.streamRunningStatus()
+            if "Stopped" in stream_status:
+                if "Overrun" in stream_status:
+                    print("\tStream interrupted due to internal device buffer filling up")
+                elif "User" in stream_status:
+                    print("\tStream Stopped")
+                else:
+                    print("\tStopped for unknown reason")
         time.sleep(1)
 
 
-# Check stream status and ensure no interruptions
-def check_stream_status(module):
-    print("Checking the stream is running (all data has been captured)")
-    stream_status = module.streamRunningStatus()
-    if "Stopped" in stream_status:
-        if "Overrun" in stream_status:
-            print("\tStream interrupted due to internal device buffer filling up")
-        elif "User" in stream_status:
-            print("\tStream Stopped")
-        else:
-            print("\tStopped for unknown reason")
-    else:
-        print("\tStream ran correctly")
+def check_header_contains_channels_to_monitor(module: quarchPPM):
+    """
+    Checks if the stream header returned from the module contains the set of channels you wish to monitor.
+    Note: List of channels to monitor is currently defined as a global variable in the script.
+    :param module: quarchPPM
+    :return: None
+    """
+    # Get the stream header as XML.
+    xml_response = module.sendCommand("stream text header")
+    while "Header Not Available" in xml_response:
+        xml_response = module.sendCommand("stream text header")
+
+    # Parse the XML
+    root = ET.fromstring(xml_response)
+
+    # Extract channels and store them as Channel objects in a dict
+    extracted_channels = {}
+    # List of Channel Keys to check against
+    for channel in root.findall(".//channel"):
+        channel_obj = Channel(
+            name=channel.find("name").text,
+            group=channel.find("group").text,
+            units=channel.find("units").text,
+            max_t_value=int(channel.find("maxTValue").text),
+            data_position=int(channel.find("dataPosition").text),
+        )
+        # Add extracted Channel object to a dict
+        extracted_channels[f"{channel_obj.name} {channel_obj.units}"] = channel_obj
+
+    # Append extracted channels from header into global channels list of dicts
+    channels.append(extracted_channels)
+
+    # Debug - output all channels.
+    # print(str(extracted_channels))
+
+    # Check if all channels to monitor are present in the stream
+    for key, value in channelsToMonitor.items():
+        if extracted_channels.get(key) is None:
+            print(f"Channel: {key} not found.")
 
 
 def process_qis_data(device_id, csv_data_io_data):
-    # Get the entire contents of the buffer as a string
-    # csv_data_string = csv_data_io_data.getvalue()
-
-    # # DEBUG - Print Header and CSV Data as a List
-    # print("\nIn-Memory Data acquired from QIS: \n")
-    #
-    # # Print all csv data (for debugging)
-    # print(csv_data_string)
-
+    """
+    Converts the in-memory python data structure that holds the stream data from QIS into a CSV file.
+    :param device_id:
+    :param csv_data_io_data:
+    :return: None
+    """
     print(f"\nProcessing Stream Data for Module: {device_id}")
 
     # Move cursor to the beginning of the StringIO object
@@ -264,8 +370,10 @@ def process_qis_data(device_id, csv_data_io_data):
 
     print(f"QIS Stream Data Saved to CSV File: {csv_file_path}\n")
 
+
 def get_device_id(device_id):
-    return device_id.replace("TCP:", "")
+    """ Strips the device identifier e.g. "TCP:" and returns the device id """
+    return device_id.split(":", 1)[1].strip()
 
 
 # Calling the main() function
